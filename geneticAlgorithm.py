a40c507f2a57753deb3975a443257ee047901e93
Lucas Orlikoski <lucas.orlikoski@mps.com.br>
2023-03-30 14:35:47 -0300

    apanhando pro AG

--- geneticAlgorithm.py
+++ geneticAlgorithm.py
@@ -19,23 +19,38 @@ def genetic_algorithm(graph, start_node, num_generations, population_size):
         # Avaliar a aptidão de cada indivíduo na população
         fitness_scores = [nx.shortest_path_length(graph, start_node, individual, weight='weight') for individual in population]
         
-        # Selecionar os indivíduos mais aptos para reprodução
-        selected_parents = random.choices(population, weights=fitness_scores, k=population_size//2)
+        # Selecionar os pais para reprodução por torneio
+        parents = []
+        for i in range(population_size//2):
+            tournament = random.sample(range(population_size), k=2)
+            if fitness_scores[tournament[0]] < fitness_scores[tournament[1]]:
+                parents.extend([population[tournament[0]], population[tournament[1]]])
+            else:
+                parents.extend([population[tournament[1]], population[tournament[0]]])
         
         # Criar nova população a partir dos pais selecionados
-        new_population = selected_parents.copy()
-        while len(new_population) < population_size:
-            parent1, parent2 = random.sample(selected_parents, 2)
-            crossover_point = random.randint(1, len(start_node)-1)
-            child = parent1[:crossover_point] + parent2[crossover_point:]
-            mutation_point = random.randint(0, len(child)-1)
-            child[mutation_point] = random.choice(list(graph.nodes()))
+        new_population = []
+        for i in range(population_size//2):
+            parent1, parent2 = parents[i*2], parents[i*2+1]
+            crossover_point = random.randint(1, len(graph)-1)
+            if crossover_point > 0:
+                child = list(str(parent1))[:crossover_point] + list(str(parent2))[crossover_point:]
+            else:
+                child = list(str(parent2))            
+            if child:
+                mutation_point = min(random.randint(1, len(child)-1), len(child)-1)
+            else:
+                mutation_point = 0
+
+            if mutation_point != 0 and mutation_point != crossover_point:
+                child[mutation_point] = random.choice(list(graph.nodes()))
+
             new_population.append(child)
         
         # Substituir a população antiga pela nova população
-        population = new_population
+        population = sorted(new_population, key=lambda individual: nx.shortest_path_length(graph, start_node, individual, weight='weight'))[:population_size]
     
     # Retornar o indivíduo mais apto na última geração
-    return min(population, key=lambda individual: nx.shortest_path_length(graph, start_node, individual, weight='weight'))
+    return population[0]
 
 genetic_algorithm(read_graph_from_file(), 9, 5, 15)
